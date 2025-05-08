3ce760af08408bcfd93469541727d7b065438391
DavidKDevui <101657192+DavidKDevui@users.noreply.github.com>
2025-05-08 19:15:58 +0200

    Ajout des carburants disponibles

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import json
 from datetime import datetime
 import geocoder
 
@@ -22,6 +21,7 @@ def get_current_location():
     if g.ok:
         return g.lat, g.lng
     return None
+
 
 
 def get_stations_around(latitude: float, longitude: float) -> dict:
@@ -49,38 +49,57 @@ def get_station_hours(days: list, current_day: str) -> dict:
             return day["TimeSlots"][0]
     return None
 
+
+def get_available_fuels(fuels: list) -> list:
+    available_fuels = []
+    for fuel in fuels:
+        if fuel["available"] == True:
+            available_fuels.append(fuel)
+            
+    if len(available_fuels) == 0:
+        return None
+    
+    return available_fuels
  
 
 
 def main():
     
-    #fuel_type = "SP98"
     print("\nChargement des 5 stations les plus proches de chez vous...")
     
     location = get_current_location()
     if location:
         lat, lng = location
-        #print(f"Votre position : {lat}, {lng}")
         stations = get_stations_around(lat, lng)
     else:
         print("Impossible de détecter votre position, utilisation des coordonnées par défaut (Paris)")
         return
+    
     
     current_day = JOURS_FR[datetime.now().strftime("%A")]
 
     if stations:
         for station in stations:
             open_hours = get_station_hours(station['Hours']['Days'], current_day)
+            available_fuels = get_available_fuels(station['Fuels'])
             
             print("\n" + "="*50)
-            print(f"Station: {station['Brand']['name']}")
+            print(f"\nStation: {station['Brand']['name']}")
             print(f"Adresse: {station['Address']['street_line']}, {station['Address']['city_line']}")
             print(f"Distance: {station['Distance']['text']}")
+            
             
             if open_hours:
                 print(f"Ouvert aujourd'hui de {open_hours['opening_time']} à {open_hours['closing_time']}")
             else:
                 print("Pas d'informations sur les horaires")
+                
+            if available_fuels:
+                print("Types de carburant disponible:")
+                for fuel in available_fuels:
+                    print(f"- {fuel['short_name']}")
+            else:
+                print("Aucun fuel disponible")
     else:
         print("Aucune station trouvée")
         
